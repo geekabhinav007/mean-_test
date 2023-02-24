6b3068bd9dc56cef41deb7b9dd36d1ec093c65af
geekabhinav007 <abhinav26january@gmail.com>
2023-02-24 15:35:03 +0530

    changed form group setting

--- frontend/mean-stack-crud-app/src/app/components/employee-edit/employee-edit.component.ts
+++ frontend/mean-stack-crud-app/src/app/components/employee-edit/employee-edit.component.ts
@@ -2,12 +2,21 @@ import { Employee } from './../../model/employee';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { ApiService } from './../../service/api.service';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+
 @Component({
   selector: 'app-employee-edit',
   templateUrl: './employee-edit.component.html',
   styleUrls: ['./employee-edit.component.css']
 })
+
+interface EmployeeForm extends FormGroup {
+  controls: {
+    name: FormControl;
+    email: FormControl;
+    designation: FormControl;
+  }
+}
 export class EmployeeEditComponent implements OnInit {
   submitted = false;
   editForm: FormGroup;
